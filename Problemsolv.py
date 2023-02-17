26fdb1e4258fe08ade38e70083381f71ad347fa1
sreya <122959564+Sreya-305@users.noreply.github.com>
2023-02-17 08:53:49 +0530

    Question is added.

--- Problemsolv.py
+++ Problemsolv.py
@@ -1,3 +1,6 @@
+#John is a recruiter in a company and wants to map candidates based on their location preferences.
+#Develop an algorithm to match candidates to the locations based on their preferences.If two candidates choose same location
+#and there is only vacancy in that location,we allocate the location based on the candidate's marks.
 class candidate:
     def __init__(self,name,marks,address,choices):
         self.name=name
